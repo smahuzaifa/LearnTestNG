4be890a36f6e8bb05be4a083c6772f1e41d0a943
smahuzaifa <smahuzaifa@localhost>
2022-07-03 12:03:21 +0530

    Helper Annotations added

---
+++ TestNGTutorial/src/test/Day5.java
@@ -0,0 +1,44 @@
+package test;
+
+import org.testng.annotations.Test;
+
+//In TestNG the flow of execution would be based on alphabetical order of method name among similar level methods
+public class Day5 {
+	@Test
+	public void WebLoginCar()
+	{
+		System.out.println("Web Login Car");
+	}
+	@Test
+	public void MobileLoginCar()
+	{
+		System.out.println("Mobile Login Car");
+	}
+	@Test(dependsOnMethods= {"WebLoginCar"})
+	//This says that this particular case is dependent on the mentioned case. So here first the mentioned method will run and
+	//then the dependent method. 1. WebLoginCar; 2.APILoginCar
+	public void APILoginCar()
+	{
+		System.out.println("API Login Car");
+	}
+	@Test(dependsOnMethods= {"WebLoginCar","MobileLoginCar"})
+	/*
+	 * This means that the above method is dependent on more than one method to execute
+	 */
+	public void APILoginCar2()
+	{
+		System.out.println("API Login Car2");
+	}
+	@Test(enabled=false) //TestNG will safely skip this case while executing. This could be due to known bug in the module and avoid
+	//a false report/failure/red flag
+	public void APILoginCar3()
+	{ 
+		System.out.println("API Login Car3");
+	}
+	@Test(timeOut=4000) //Will not fail until 40seconds before throwing and error
+	//Can be used when the particular execution is taking time. So we say hold on for mentioned time
+	public void APILoginCar4()
+	{
+		System.out.println("API Login Car4");
+	}
+}
